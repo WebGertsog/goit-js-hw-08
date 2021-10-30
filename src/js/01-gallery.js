49b68d0edf5c7ac944bbcf8022d640446e5f9fd0
WebGertsog <groxenvil@gmail.com>
2021-10-30 19:44:09 +0300

    task-01
    task-01 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,20 @@
+import galleryCardsTpl from '../templates/gallery-cards.hbs';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const imagesMarcap = createGallerysMarkap(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', imagesMarcap);
+
+
+function createGallerysMarkap (galleryItems){
+  return galleryCardsTpl(galleryItems);
+}
+
+const lightBox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250});
